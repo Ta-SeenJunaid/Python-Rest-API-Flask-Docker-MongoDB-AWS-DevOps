bd882a1cebee182ea65a82e232b2b0ab3fe8cbc3
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-08-10 14:02:12 +0600

    User Signup

--- Database as a Service/application/app.py
+++ Database as a Service/application/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
+import bcrypt
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,3 +9,30 @@ api = Api(app)
 client = MongoClient("mongodb://db:27017")
 db = client.SentencesDatabase
 users = db["Users"]
+
+class Register(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+
+        hashed_pw = bcrypt.hashpw(password, bcrypt.gensalt())
+
+        users.insert({
+            "Username": username,
+            "Password": hashed_pw,
+            "Sentence": ""
+        })
+
+        retJson = {
+            "status": 200,
+            "msg": "You successfully signed up for the API"
+        }
+
+        return jsonify(retJson)
+
+api.add_resource(Register, '/register')
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
