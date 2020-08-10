5ca49a5ef92d46cd53e108d105ed40ee2bf001ac
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-08-10 11:53:18 +0600

    Initial setup

---
+++ Database as a Service/application/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+
+app = Flask(__name__)
+api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+db = client.SentencesDatabase
+users = db["Users"]
