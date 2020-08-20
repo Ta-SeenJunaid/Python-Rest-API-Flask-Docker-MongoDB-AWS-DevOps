1285318c42f26d148ba78c0fa00fc0f861f4d6d8
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-08-20 12:29:41 +0600

    Bug fixing

--- Database as a Service/application/app.py
+++ Database as a Service/application/app.py
@@ -6,13 +6,39 @@ import bcrypt
 app = Flask(__name__)
 api = Api(app)
 
-client = MongoClient("mongodb://db:27017")
+client = MongoClient("mongodb://database:27017")
 db = client.SentencesDatabase
 users = db["Users"]
 
+class Register(Resource):
+    def post(self):
+        #Step 1 is to get posted data by the user
+        postedData = request.get_json()
+
+        #Get the data
+        username = postedData["username"]
+        password = postedData["password"] #"123xyz"
+
+
+        hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+
+        #Store username and pw into the database
+        users.insert({
+            "Username": username,
+            "Password": hashed_pw,
+            "Sentence": "",
+            "Tokens":6
+        })
+
+        retJson = {
+            "status": 200,
+            "msg": "You successfully signed up for the API"
+        }
+        return jsonify(retJson)
+
 def verifyPw(username, password):
     hashed_pw = users.find({
-        "Username": username
+        "Username":username
     })[0]["Password"]
 
     if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed_pw:
@@ -22,79 +48,59 @@ def verifyPw(username, password):
 
 def countTokens(username):
     tokens = users.find({
-        "Username": username
+        "Username":username
     })[0]["Tokens"]
     return tokens
 
-class Register(Resource):
-    def post(self):
-        postedData = request.get_json()
-
-        username = postedData["username"]
-        password = postedData["password"]
-
-        hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-
-        users.insert({
-            "Username": username,
-            "Password": hashed_pw,
-            "Sentence": "",
-            "Tokens": 10
-        })
-
-        retJson = {
-            "status": 200,
-            "msg": "You successfully signed up for the API"
-        }
-
-        return jsonify(retJson)
-
 class Store(Resource):
     def post(self):
-
+        #Step 1 get the posted data
         postedData = request.get_json()
 
+        #Step 2 is to read the data
         username = postedData["username"]
         password = postedData["password"]
         sentence = postedData["sentence"]
 
+        #Step 3 verify the username pw match
         correct_pw = verifyPw(username, password)
 
         if not correct_pw:
             retJson = {
-                "status":302,
-                "msg": "Wrong Username or Password"
+                "status":302
             }
-
             return jsonify(retJson)
-
+        #Step 4 Verify user has enough tokens
         num_tokens = countTokens(username)
-        if num_tokens <=0:
+        if num_tokens <= 0:
             retJson = {
-                "status": 301,
-                "msg": "Out of token"
+                "status": 301
             }
-
             return jsonify(retJson)
 
+        #Step 5 store the sentence, take one token away  and return 200OK
         users.update({
             "Username":username
         }, {
             "$set":{
                 "Sentence":sentence,
                 "Tokens":num_tokens-1
-            }
+                }
         })
 
         retJson = {
             "status":200,
-            "msg": "Sentence saved successfully"
+            "msg":"Sentence saved successfully"
         }
         return jsonify(retJson)
 
 
+
+
+
 api.add_resource(Register, '/register')
 api.add_resource(Store, '/store')
+
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
